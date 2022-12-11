299922663d451ed5144cca9542f4efe9b652f82b
Hemanth Katte <hemanthss24@gmail.com>
2022-12-11 23:37:57 +0530

    encapsulation demonstration

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly r = new RWOnly();
+      //r.name = Ram;
+      //System.out.println(r.name);
+      //java: name has private access in com.driver.RWOnly
+      r.setName("Ram");
+      String ret =  r.getName();
+      System.out.println(ret);
+  }
 }
